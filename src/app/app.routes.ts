bc6ef01e6edf92086ff27b8e6f1373d5fd49f6c7
Kariqs <kariukibenard189@gmail.com>
2026-01-15 17:21:24 +0300

    Added signup page.

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,8 +2,10 @@ import { Routes } from '@angular/router';
 import { Dashboard } from './pages/dashboard/dashboard';
 import { Home } from './pages/home/home';
 import { Features } from './pages/features/features';
+import { Signup } from './pages/auth/signup/signup';
 
 export const routes: Routes = [
+  { path: 'signup', component: Signup },
   { path: '', component: Home },
   { path: 'dashboard', component: Dashboard },
   { path: 'features', component: Features },
