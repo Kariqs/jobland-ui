71f44183a1ffaa0f0c2dbc636d32af239a96c1f3
Kariqs <kariukibenard189@gmail.com>
2026-01-16 16:08:19 +0300

    Added account activation.

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -5,10 +5,12 @@ import { Features } from './pages/features/features';
 import { Signup } from './pages/auth/signup/signup';
 import { Login } from './pages/auth/login/login';
 import { authGuard } from './services/guards/auth-guard';
+import { AccountActivationComponent } from './pages/auth/activate/activate';
 
 export const routes: Routes = [
   { path: 'signup', component: Signup },
   { path: 'login', component: Login },
+  { path: 'activate', component: AccountActivationComponent },
   { path: '', component: Home },
   { path: 'dashboard', component: Dashboard, canActivate: [authGuard] },
   { path: 'features', component: Features },
